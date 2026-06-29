dba7ff749d3f2f1e85467b164afabbae4a5bf222
Brian N. White <bnwhite@demon-ln1.demon.wfubmc.edu>
2026-06-28 21:00:26 -0400

    fix filepaths in slurm file

---
+++ OutFiles/my_computation-836773.e
@@ -0,0 +1,4 @@
+Error in checkForRemoteErrors(val) : 
+  3 nodes produced errors; first error: cannot open the connection
+Calls: parLapply ... clusterApply -> staticClusterApply -> checkForRemoteErrors
+Execution halted
--- WAprevalence/code/WA_abundance_model_parallel.R
+++ WAprevalence/code/WA_abundance_model_parallel.R
@@ -26,4 +26,6 @@ chains <- parLapply(cl, 1:3, run_chain, wd = wd)
 stopCluster(cl)
 
 samples <- do.call(mcmc.list, chains)
+
+dir.create("WAprevalence/output/mcmc", recursive = T, showWarnings = F)
 save(samples, file = "WAprevalence/output/mcmc/MCMC_N_pois_3_chains_2_mill_each_2026_06_28.Rda")
